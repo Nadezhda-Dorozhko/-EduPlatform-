a18c4e77e1863ae3ac21e88a1aeca73033b039fc
nadya <nadeya.dor@gmail.com>
2026-03-09 21:05:05 +0300

    feat: implement responsive layout according to Figma template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Сообщение в консоли
-console.log("JavaScript подключен успешно");
+console.log('JavaScript подключен успешно');
 
 // Приветственное сообщение
-alert("Добро пожаловать!");
+alert('Добро пожаловать!');
+const burger = document.querySelector('.header__burger');
+const nav = document.querySelector('.header__nav');
+
+if (burger && nav) {
+  burger.addEventListener('click', () => {
+    const isOpen = nav.classList.toggle('is-open');
+    burger.setAttribute('aria-expanded', isOpen);
+  });
+}
